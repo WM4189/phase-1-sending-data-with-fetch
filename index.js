317415b6020577ee327b85d038ddbdd76fe48613
William Mosto <will.mosto@gmail.com>
2021-08-18 09:46:14 -0400

    commit tree 5fa9289d1ead27d73ba3096d2bbfb3c4cbfa2d0e

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(userName, userEmail){
+    
+return fetch('http://localhost:3000/users',{
+        method : 'POST',
+        headers : {
+            'Content-Type': 'application/json',
+            'Accept' : 'application/json'
+        },
+        body:JSON.stringify({ name: userName, email:userEmail})
+    })
+    .then(res=> res.json())
+    .then(data => {
+        const div = document.createElement('div')
+        const ul = document.createElement('ul')
+        document.body.appendChild(div)
+        div.appendChild(ul)
+        ul.append(data.id)
+        console.log(data)
+    })
+    .catch(error => {
+
+        document.body.append(error)
+    } )
+}
+// submitData('Billy', 'www.BillBo.com')
